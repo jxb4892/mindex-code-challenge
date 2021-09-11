468d56e8493297318dce2199b7e4358715e84de4
jxb4892 <jxb4892@rit.edu>
2021-09-11 11:27:43 -0600

    data bootstrap test for compensation repo

--- src/test/java/com/mindex/challenge/DataBootstrapTest.java
+++ src/test/java/com/mindex/challenge/DataBootstrapTest.java
@@ -1,15 +1,18 @@
 package com.mindex.challenge;
 
+import java.util.List;
+import java.text.SimpleDateFormat;
 import com.mindex.challenge.dao.EmployeeRepository;
 import com.mindex.challenge.dao.CompensationRepository;
 import com.mindex.challenge.data.Employee;
+import com.mindex.challenge.data.Compensation;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.junit4.SpringRunner;
 import static org.junit.Assert.assertEquals;
-
+import static org.junit.Assert.assertNotEquals;
 import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
@@ -30,5 +33,12 @@ public class DataBootstrapTest {
         assertEquals("Lennon", employee.getLastName());
         assertEquals("Development Manager", employee.getPosition());
         assertEquals("Engineering", employee.getDepartment());
+
+        SimpleDateFormat formatter = new SimpleDateFormat("yyyy/MM/dd");
+        List<Compensation> compensation = compensationRepository.findByEmployee_EmployeeId("16a596ae-edd3-4847-99fe-c4518e82c86f");
+        assertNotEquals(compensation.size(), 0);
+        assertEquals(89565.45, compensation.get(0).getSalary(), 0.0);
+        String actualDate = formatter.format(compensation.get(0).getEffectiveDate().getTime());
+        assertEquals("2023/12/31", actualDate);
     }
 }
