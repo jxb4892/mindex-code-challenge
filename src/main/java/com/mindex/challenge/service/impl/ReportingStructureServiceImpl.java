b0825f451787cf02e6821e416e9b1c5b2e3b4db8
jxb4892 <jxb4892@rit.edu>
2021-09-10 22:44:41 -0600

    testing error - rest template's exchange and PUT not working

--- src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
+++ src/main/java/com/mindex/challenge/service/impl/ReportingStructureServiceImpl.java
@@ -3,6 +3,7 @@ package com.mindex.challenge.service.impl;
 import java.util.List;
 import java.util.HashSet;
 import com.mindex.challenge.dao.EmployeeRepository;
+import com.mindex.challenge.dao.ReportingStructureRepository;
 import com.mindex.challenge.data.Employee;
 import com.mindex.challenge.data.ReportingStructure;
 import com.mindex.challenge.service.ReportingStructureService;
@@ -10,7 +11,6 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-import java.util.UUID;
 
 @Service
 public class ReportingStructureServiceImpl implements ReportingStructureService {
@@ -20,6 +20,9 @@ public class ReportingStructureServiceImpl implements ReportingStructureService 
     @Autowired
     private EmployeeRepository employeeRepo;
 
+    @Autowired
+    private ReportingStructureRepository rsRepo;
+
     private HashSet<String> reporterSet;
 
     @Override
@@ -27,6 +30,7 @@ public class ReportingStructureServiceImpl implements ReportingStructureService 
         LOG.debug("Getting a reporting structure of employee with id [{}]", id);
 
         Employee requestedEmployee = employeeRepo.findByEmployeeId(id);
+        LOG.debug("Found employee " + requestedEmployee.getFirstName());
 
         if (requestedEmployee == null) {
             throw new RuntimeException("Invalid employeeId: " + id);
@@ -37,27 +41,32 @@ public class ReportingStructureServiceImpl implements ReportingStructureService 
         if (reporterSet.contains(id)) {
             numReports--;
         }
-
+        LOG.debug("got the num of reports?");
         ReportingStructure structure = new ReportingStructure();
         structure.setEmployee(requestedEmployee);
-        System.out.println("hello???????");
-        System.out.println(structure.getEmployee().getFirstName());
         structure.setNumberOfReports(numReports);
 
+        LOG.debug("Found number of reports: " + numReports);
+        reporterSet.clear();
         return structure;
     }
 
     private int getReports(Employee employee) {
+        LOG.debug("getReports invoked: " + employee.getFirstName());
         if (reporterSet.contains(employee.getEmployeeId())) {
             return 0;
         }
         List<Employee> reports = employee.getDirectReports();
+        LOG.debug("where it will go wrong ");
         if (reports == null || reports.size() == 0) {
             reporterSet.add(employee.getEmployeeId());
             return 1;
         }
         int totalReports = 0;
+        LOG.debug("initializing total reports");
         for (Employee reporter : reports) {
+            LOG.debug("current top " + employee.getFirstName());
+            LOG.debug("got reporter " + reporter.getFirstName());
             reporter = employeeRepo.findByEmployeeId(reporter.getEmployeeId());
             totalReports += getReports(reporter);
         }
